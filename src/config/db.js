a0fc2d98d281134af267787a5c05c72578a351bc
Edwin Marulanda <roed2610@gmail.com>
2025-08-20 14:15:32 -0500

    Ajuste conexión base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "postgres",
-  logging: false, // Para evitar logs innecesarios en la consola
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false, // Evita logs innecesarios
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // 👈 necesario en Render
+      },
+    },
+  }
+);
 
 export default sequelize;
